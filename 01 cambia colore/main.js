a8fa54c3bf6316d2e9648cdf15d3b1053384ca8d
bonvi83 <bonvi83@gmail.com>
2024-02-10 18:47:59 +0100

    aggiunte le console.log

--- 01 cambia colore/main.js
+++ 01 cambia colore/main.js
@@ -1,16 +1,19 @@
 function clickBottone (){
 
-    // ogni volta che clicco stampa click
-    console.log("click");
-
     // assegno dei parametri al valore max e min
     let max = 255;
     let min = 0;
 
     // genero 3 numeri casuali, da 0 a 255, per i 3 colori dell'rgb
+    // ogni volta che clicco sul bottone mi stampa i tre valori
     let red = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log("red " + red)
+
     let green = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log("green " + green)
+
     let blue = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log("blue " + blue)
 
     // assegno questi colori al background del body
     document.body.style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")"
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-## Prove e riprove con JS
+## Mini progetti JS
 
-File creato per mettermi alla prova con JS, cercando di capire e replicare piccoli progettini che trovo in giro per il web (nulla di trascendentale).
+File creato per mettermi alla prova con JS, cercando di capire e replicare piccoli progettini che trovo in giro per il web e magari qualcosa che invento io (nulla di trascendentale ovviamente).
 
 **Esercizio 1**
 
-Creo un bottone che al click cambia lo sfondo della pagina html e stampa in console.log la parola click.
+Creo un bottone che al click cambia lo sfondo della pagina html e stampa in console.log il valore dei tre colori.
